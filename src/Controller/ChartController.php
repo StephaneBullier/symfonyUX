1f9faa1483820d93d267657b110fd024895f78d6
steph <sbullier@gmail.com>
2020-12-11 15:43:58 +0100

    Installation de ux-chartjs Add first example

---
+++ src/Controller/ChartController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
+
+class ChartController extends AbstractController
+{
+    /**
+     * @Route("/chart", name="chart")
+     * @param ChartBuilderInterface $chartBuilder
+     * @return Response
+     */
+    public function index(ChartBuilderInterface $chartBuilder): Response
+    {
+        $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
+        $chart->setData([
+            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            'datasets' => [
+                [
+                    'label' => 'My First dataset',
+                    'backgroundColor' => 'rgba(54, 162, 235, 0.2)',
+                    'borderColor' => 'rgb(255, 99, 132)',
+                    'data' => [0, 10, 5, 2, 20, 30, 45],
+                ],
+            ],
+        ]);
+
+
+        return $this->render('chart/index.html.twig', [
+            'chart' => $chart
+        ]);
+    }
+}
