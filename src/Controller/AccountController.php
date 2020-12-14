4d93e015dfeadf9ceb2fa11d6c28f74535b26c75
steph <sbullier@gmail.com>
2020-12-14 17:13:18 +0100

    Create functionnality to add profil photo for user

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Repository\UserRepository;
+use App\Form\UserFormType;
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,18 +24,45 @@ class AccountController extends AbstractController
 
     /**
      * @Route("/account/edit/{id}", name="app_account_edit")
+     * @param int $id
+     * @param Request $request
+     * @param EntityManagerInterface $entityManager
+     * @param string $photoDir
      * @return Response
+     * @throws Exception
      */
-    public function edit(int $id, UserRepository $userRepository): Response
+    public function edit(int $id, Request $request, EntityManagerInterface $entityManager, string $photoDir): Response
     {
-        $user = $userRepository->find($id);
+        /* @var User $user */
+        $user = $this->getUser();
 
         if (!$user) {
             throw $this->createNotFoundException('Aucun utilisateur trouvé pour l\'id '.$id);
         }
 
+        $form = $this->createForm(UserFormType::class, $user);
 
+        $form->handleRequest($request);
 
-        return $this->render('account/edit.html.twig');
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($photo = $form['photo']->getData()) {
+                $filename = bin2hex(random_bytes(6)).'.'.$photo->guessExtension();
+                try {
+                    $photo->move($photoDir, $filename);
+                } catch (FileException $e) {
+                    throw new FileException('Erreur lors de la copie du fichier');
+                }
+                $user->setPhotoFilename($filename);
+            }
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Votre photo de profil a bien été enregistrée.');
+
+            return $this->redirectToRoute('app_account');
+        }
+
+        return $this->render('account/edit.html.twig', [
+            'userForm' => $form->createView()
+        ]);
     }
 }
